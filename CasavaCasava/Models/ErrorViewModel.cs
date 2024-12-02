772f935009be6671c8adc160b469db023115f153
AaronCodes4 <aaronapraku026@gmail.com>
2024-12-02 16:05:25 +0000

    Add project files.

---
+++ CasavaCasava/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CasavaCasava.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
